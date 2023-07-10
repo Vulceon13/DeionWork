85d1cda227d311de6b4a3a93a0f4dcb7171761fb
Deion Trinidad <deion_trinidad@outlook.com>
2023-07-10 17:02:09 +1000

    Working on file code

---
+++ ActivitiesOnFiles/ActivitiesOnFiles/ActivitiesOnFiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace ActivitiesOnFiles
+{
+    internal class ActivitiesOnFiles
+    {
+        static void Main()
+        {
+            string[] array;
+            StreamReader reader = new StreamReader(@"../../Words.txt");
+            string snumber = reader.ReadLine();
+            int n = int.Parse(snumber);
+
+            array = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = reader.ReadLine();
+            } 
+            reader.Close();
+
+            
+            
+
+            StreamWriter writer = new StreamWriter(@"../../Output.txt");
+            writer.WriteLine("");
+            writer.Close();
+            Console.ReadKey();
+        }
+    }
+}
